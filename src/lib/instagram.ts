b3d11b1f3d6072a4710450f9a3d0d34dc1a865d4
GlavaNet <christopher.glavan@gmx.com>
2025-02-06 13:42:39 -0800

    CORS proxy for IG API

--- src/lib/instagram.ts
+++ src/lib/instagram.ts
@@ -10,7 +10,9 @@ export const fetchInstagramPosts = async (): Promise<InstagramPost[]> => {
   
   try {
     const response = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://graph.instagram.com/v12.0/${userId}/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${token}&limit=6`,
+      `https://api.allorigins.win/get?url=${encodeURIComponent(
+        `https://cors-anywhere.herokuapp.com/https://graph.instagram.com/v12.0/${userId}/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${token}&limit=6`
+      )}`
     );
     const data = await response.json();
     return data.data || [];
