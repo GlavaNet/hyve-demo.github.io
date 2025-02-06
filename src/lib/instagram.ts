0b15aa4d90efee8db3dd0d36700eeb3e189dc22f
GlavaNet <christopher.glavan@gmx.com>
2025-02-06 13:18:19 -0800

    update IG API handling

--- src/lib/instagram.ts
+++ src/lib/instagram.ts
@@ -10,7 +10,14 @@ export const fetchInstagramPosts = async (): Promise<InstagramPost[]> => {
   
   try {
     const response = await fetch(
-      `https://graph.instagram.com/v12.0/${userId}/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${token}`
+      `https://graph.instagram.com/v12.0/${userId}/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${token}&limit=6`,
+      {
+	      headers: {
+		      'Accept': 'application/json',
+		      'Access-Control-Allow-Origin': '*'
+	      },
+	      mode: 'cors'
+      }
     );
     const data = await response.json();
     return data.data || [];
