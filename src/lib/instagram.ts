99589c080dfcbd0a889b4c38c10b798e848ceeda
GlavaNet <christopher.glavan@gmx.com>
2025-02-12 11:55:43 -0800

    Fix syntax in instagram.ts

--- src/lib/instagram.ts
+++ src/lib/instagram.ts
@@ -14,7 +14,7 @@ export const fetchInstagramPosts = async (): Promise<InstagramPost[]> => {
         // `https://cors-anywhere.herokuapp.com/https://graph.instagram.com/v12.0/${userId}/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${token}&limit=6`
       // )}`
       'https://instagram-proxy.sabrastrain3.workers.dev/'
-    )};
+    );
     const data = await response.json();
     return data.data || [];
   } catch (error) {
